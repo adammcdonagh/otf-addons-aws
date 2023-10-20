4b4bbeda42a210331e8586f309950667e036a251
Adam McDonagh <27215476+adammcdonagh@users.noreply.github.com>
2023-10-20 11:00:53 +0000

    Try fix env vars again

--- tests/fixtures/moto.py
+++ tests/fixtures/moto.py
@@ -10,4 +10,5 @@ def credentials_moto():
     os.environ["AWS_SECURITY_TOKEN"] = "testing"
     os.environ["AWS_SESSION_TOKEN"] = "testing"
     os.environ["AWS_DEFAULT_REGION"] = "eu-west-1"
-    del os.environ["AWS_ENDPOINT_URL"]
+    if os.environ.get("AWS_ENDPOINT_URL"):
+        del os.environ["AWS_ENDPOINT_URL"]
